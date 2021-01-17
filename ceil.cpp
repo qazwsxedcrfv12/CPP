309e322df652febc2aab7dca774f2d5861fac2b7
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2021-01-18 01:40:50 +0530

    Ceil and Floor of a no.

---
+++ ceil.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fastIO(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+}
+
+int main()
+{
+   fastIO();
+   
+   int a=5,b=2;
+   cout<<floor(a/b)<<endl;
+   cout<<ceil(1.00*a/b)<<endl;
+  return 0;
+}
