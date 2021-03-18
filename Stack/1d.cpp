ae5ad8511b2eecdf40c24c72cfd4c36921e96607
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2021-03-18 23:31:18 +0530

    Nearest greater to left

--- Stack/1d.cpp
+++ Stack/1d.cpp
@@ -1,1 +1,47 @@
 
+                                          // Nearest greater to left
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> search(int arr[], int n)
+{
+   stack<int>s;
+   vector<int>v;
+   
+   for(int i=0; i<n; i--)
+   {
+       if(s.size()==0)
+        v.push_back(-1);
+       else if(s.size()>0 && s.top()>arr[i])
+        v.push_back(s.top());
+       else if(s.size()>0 && s.top()<=arr[i])
+       {
+           while(s.size()>0 && s.top()<=arr[i])
+             s.pop();
+           if(s.size()==0)
+            v.push_back(-1);
+           else
+            v.push_back(s.top());
+       }
+      s.push(arr[i]);
+   }
+  return v;
+}
+
+int main()
+{
+    vector<int>v;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+       cin>>arr[i];
+    
+    v=search(arr,n);
+    
+    for(auto it: v)
+      cout<<it<<" ";
+     cout<<"\n";
+  return 0;
+}
