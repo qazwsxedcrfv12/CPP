c1412b2f992cf15761a7db7f2552979cf942103d
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2021-04-05 00:47:17 +0530

    Postorder Traversal

---
+++ Trees/Tree Traversals/3.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct Node{
+   int data;
+   Node* left;
+   Node* right;
+};
+
+
+Node* getnewNode(int data)                  //Creating a new node
+{
+    Node* root=new Node();
+    root->data=data;
+    root->left=NULL;
+    root->right=NULL;
+    
+    return root;
+}
+
+Node* insert(Node* root, int data)          //Inserting a new node to tree
+{
+   if(root==NULL)
+     root=getnewNode(data);
+   else if(data<=root->data)
+     root->left=insert(root->left,data);
+   else
+     root->right=insert(root->right,data);
+   return root;
+}
+
+void print(Node* root)             //Processing the nodes of tree in DFS(postorder) strategy
+{
+    if(root==NULL)
+      return;
+    print(root->left);
+    print(root->right);
+    cout<<root->data<<" ";
+}
+
+int main()
+{
+    Node* root=NULL;
+    int n,a;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        cin>>a;
+        root=insert(root,a);
+    }
+  
+    print(root);
+   
+   cout<<"\n";
+ return 0;
+}
