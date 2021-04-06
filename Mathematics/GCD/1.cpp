6eb98b31dfcd76cdb577767288af951433480ad6
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2021-04-06 12:14:29 +0530

    Naive sol. to find GCD

--- Mathematics/GCD/1.cpp
+++ Mathematics/GCD/1.cpp
@@ -1,1 +1,27 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+
+#define f0 ios_base::sync_with_stdio(false); cin.tie(0);
+
+int find_gcd(int a, int b)
+{
+    int res=min(a,b);
+    while(res>0)
+    {
+       if(a%res==0 && b%res==0)
+        break;
+       res--;
+    }
+   return res;
+}
+
+int main()
+{
+    f0;
+    int a,b;
+    cin>>a>>b;
+    int c=find_gcd(a,b);
+    cout<<c<<"\n";
+  return 0;
+}
