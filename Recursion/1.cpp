938fbb01aa63799d2b3a835d58d4eebede109859
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2021-04-13 01:42:54 +0530

    Print 1 to n using recursion

--- Recursion/1.cpp
+++ Recursion/1.cpp
@@ -1,1 +1,25 @@
 
+                                       //Print 1 to n using Recursion
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define f0 ios_base::sync_with_stdio(false); cin.tie(0)
+
+void print(int n)
+{
+    if(n==0)
+      return;
+    print(n-1);
+    cout<<n<<" ";
+}
+
+int main()
+{
+  f0;
+  int n;
+  cin>>n;
+  print(n);
+  cout<<"\n";
+ return 0;
+}
