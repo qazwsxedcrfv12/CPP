71dccde7823aa8691f17dbc6da1b3b51f9c5db37
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2021-05-19 17:16:21 +0530

    Update 1.cpp

--- Sorting/Bubble-Sort/1.cpp
+++ Sorting/Bubble-Sort/1.cpp
@@ -1,1 +1,34 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int arr[], int n)
+{
+   for(int r = 1; r <= n-1; r++)
+   {
+     for(int i = 0; i <= n-1-r; i++)
+     {
+        if(arr[i] > arr[i+1])
+        {
+           t = arr[i];
+                    arr[i] = arr[i+1];
+           arr[i+1] = t;
+        }
+     }
+  }
+}
+
+int main()
+{
+   int n;
+          cin>>n;
+   int arr[n];
+   for(int i = 0; i < n; i++)
+     cin>>arr[i];
+     
+   bubble_sort(arr,n);                   // call bubble sort function
+   
+   for(int i = 0; i < n; i++)
+     cout<<arr[i]<<" ";                  // Print the array elements after sorting
+   return 0;
+}
