940286e4655e7b85e16d7842f92d257bb170fb87
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2021-06-19 17:25:57 +0530

    Using Vector

--- Bit Manipulation/Decimal-Binary/3.cpp
+++ Bit Manipulation/Decimal-Binary/3.cpp
@@ -1,1 +1,29 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+
+#define f0 ios_base::sync_with_stdio(false); cin.tie(0)    //Fast I/O 
+
+int main()
+{
+   f0;
+   vector<int>v;
+   
+   int n;
+   cin>>n;
+   
+   
+   while(n!=0)
+   {
+      v.push_back(n%2);
+      n/=2;
+   }
+   
+  reverse(v.begin(),v.end());
+  
+  for(auto it: v)
+   cout<<it;
+   
+  cout<<"\n";
+ return 0;
+}
