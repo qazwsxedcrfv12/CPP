f61b2e787f884e4d74a40fd0531de5c9315cbdd4
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2021-06-20 00:12:22 +0530

    swap value of 2 variable

--- Bit Manipulation/swap.cpp
+++ Bit Manipulation/swap.cpp
@@ -1,1 +1,20 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+
+#define f0 ios_base::sync_with_stdio(false); cin.tie(0)
+
+int main()
+{
+   f0;
+   
+   int a,b;
+   cin>>a>>b;
+   
+   a^=b;
+   b^=a;
+   a^=b;
+   
+   cout<<"The value of a and b are "<<a<<" "<<b<<"\n";
+  return 0;
+}
