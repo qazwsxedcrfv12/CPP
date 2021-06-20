d302cc4449047fee9d4520a1a272da6f51d818c6
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2021-06-20 13:59:23 +0530

    Update c.cpp
    If Array is not sorted and also contains Duplicates

--- Sorting/Intersection-of-Array/c.cpp
+++ Sorting/Intersection-of-Array/c.cpp
@@ -1,1 +1,45 @@
+//If Array is not sorted and also contains Duplicates
+//Time Complexity O(max(n,m))
+//Extra Auxilary Space O(min(n,m))
 
+// Code
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define f0 ios_base::sync_with_stdio(false); cin.tie(0)
+
+
+int main()
+{
+   
+   f0;
+   
+   int n,m;
+   cin>>n>>m;
+   int a[n],b[m];
+   
+   for(int i=0; i<n; i++)
+      cin>>a[i];
+   for(int i=0; i<m; i++)
+      cin>>b[i];
+
+   unordered_set<int>s(b.brgin(),b.end());   //if m is small  
+ // unordered_set<int>s(a.brgin(),a.end());   //if m is small 
+ 
+  for(int i=0; i<n; i++)               //if m is small
+  {
+     if(s.find(a[i])!=s.end())
+     { cout<<a[i]<<" "; s.erase(a[i]); }
+  }
+  
+ /* for(int i=0; i<m; i++)              //if n is small
+  {
+     if(s.find(b[i])!=s.end())
+     { cout<<b[i]<<" "; s.erase(b[i]); }
+  } */
+   
+   cout<<"\n";
+ return 0;
+}
