e5256cfbd911753dc498796985a3f8c1304c3410
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2021-06-20 13:29:48 +0530

    Update a.cpp
    If the array does not contain Duplicates.

--- Sorting/Intersection-of-Array/a.cpp
+++ Sorting/Intersection-of-Array/a.cpp
@@ -1,1 +1,38 @@
+// If the array does not contain Duplicates
+//Time Complexity O(n)
+//Extra Auxilary Space O(1)
 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void find_intersection(int a[], int b[], int n, int m)
+{
+   int i=0,j=0;
+   while(i<n && j<m)
+   {
+      if(a[i]==b[j])
+      { cout<<a[i++]; j++; }
+      else if(a[i]<b[j])
+       i++;
+      else
+       j++;
+   }
+}
+
+int main()
+{
+   int n,m;
+   cin>>n>>m;
+   int a[n],b[m];
+   
+   for(int i=0; i<n; i++)
+      cin>>a[i];
+   for(int i=0; i<m; i++)
+      cin>>b[i];
+  
+   find_intersection(a,b,n,m);
+   
+   cout<<"\n";
+ return 0;
+}
