65cc0267f17c2ac5eed043e7d108ab8d3c5518ce
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2021-06-20 14:00:44 +0530

    Create floor.cpp

---
+++ floor.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fastIO(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+}
+
+int main()
+{
+   fastIO();
+   
+   int a=5,b=2;
+   cout<<floor(a/b)<<endl;
+  return 0;
+}
