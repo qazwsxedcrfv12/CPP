d6ac1aa1b0ac5ace9eb4e10162756b212d54cd03
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2021-06-23 13:30:25 +0530

    Max Heap

--- Sorting/HeapSort/b.cpp
+++ Sorting/HeapSort/b.cpp
@@ -1,1 +1,39 @@
+//Sort the array in decreasing order ( max heap )
 
+#include<bits/stdc++.h>
+using namespace std;
+
+#define f0 ios_base::sync_with_stdio(false); cin.tie(0)
+#define pb v.push_back
+
+void sort_it(vector<int>v)
+{
+   priority_queue<int>q;
+   for(int i=0; i<v.size(); i++)
+      q.push(v[i]);
+   while(!q.empty())
+   {
+      cout<<q.top()<<" ";
+      q.pop();
+   }
+}
+   
+
+int main()
+{
+   f0;
+   vector<int>v;
+   int n,a;
+   cin>>n;
+   
+   for(int i=0; i<n; i++)
+   {
+       cin>>a;
+       pb(a);
+   }
+   
+      sort_it(v);
+      
+  cout<<"\n";
+ return 0;
+}
