af7c271e9131993a2c28dc89e79dc823370090fc
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2021-06-28 16:01:33 +0530

    Update a.cpp
    Using loop and swapping

--- DSA-450-Ques/Strings/ReverseString/a.cpp
+++ DSA-450-Ques/Strings/ReverseString/a.cpp
@@ -1,1 +1,30 @@
 
+//Reverse the string
+//Time complexity O(lengthof(string))
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define f0 ios_base::sync_with_stdio(false); cin.tie(0)
+
+string reverse_string(string str)
+{
+   int l=str.length();
+   for(int i=0; i<l/2; i++)
+     swap(str[i],str[l-i-1]);
+  return str;
+}
+      
+
+int main()
+{
+   f0;
+   string str;
+   cin>>str;
+   
+   str=reverse_string(str);
+   cout<<str<<"\n";
+  return 0;
+}
