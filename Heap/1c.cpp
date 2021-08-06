e507ab074fa95b38c29904200ff8660014047290
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2021-08-06 20:09:25 +0530

    Sort k sorted Array

--- Heap/1c.cpp
+++ Heap/1c.cpp
@@ -1,1 +1,35 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> insert(int arr[], int n, int k)
+{
+   vector<int>v;
+   priority_queue<int,vector<int>,greater<int>>q;
+   for(int i=0; i<n; i++)
+   {
+       q.push(arr[i]);
+       if(q.size()>k)
+       { v.push_back(q.top()); q.pop(); }
+   }
+   while(!q.empty())
+   { v.push_back(q.top()); q.pop(); }
+  return v;
+}
+       
+        
+
+int main()
+{
+  int n,k;
+  cin>>n>>k;
+  int arr[n];
+  
+  for(int i=0; i<n; i++)
+     cin>>arr[i];
+  vector<int>v=insert(arr,n,k);
+  for(auto it: v)
+     cout<<it<<" ";
+  cout<<"\n";
+ return 0;
+}
